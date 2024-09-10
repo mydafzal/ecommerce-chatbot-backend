6871f398aa52b2d0c039f2264a89c49615c740e8
mydafzal <momalafzal20@gmail.com>
2024-09-11 01:52:27 +0500

    feat: include user auth status with each new message

--- src/services/chats.service.ts
+++ src/services/chats.service.ts
@@ -1,15 +1,24 @@
+import { UserDetails } from "../dtos";
 import { generateAgentResponse } from "../langchain/agents/base-agent";
 import { generateChatId } from "../utils/helpers";
 
-async function createChat(message: string) {
+async function createChat(message: string, userDetails?: UserDetails) {
   const chatId = generateChatId();
-  const agentResponse = await generateAgentResponse(chatId, message);
+  const agentResponse = await generateAgentResponse(
+    chatId,
+    message,
+    userDetails
+  );
 
   return { chatId, agentResponse };
 }
 
-async function sendMessage(chatId: string, message: string) {
-  return await generateAgentResponse(chatId, message);
+async function sendMessage(
+  chatId: string,
+  message: string,
+  userDetails?: UserDetails
+) {
+  return await generateAgentResponse(chatId, message, userDetails);
 }
 
 export const ChatsService = { createChat, sendMessage };
